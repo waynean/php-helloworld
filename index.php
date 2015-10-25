b65945f976ca538e2a707e0ca3053d1627ee4575
waynean <anwayne@gmail.com>
2015-10-25 09:40:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
  </body>
 </html>
